22c38b4709380e84cf1a65643b3805e9d7c56c25
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-03-25 15:44:41 +0300

    done with the connection...everythng works as expected..cheeers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,37 +2,19 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 
-const list = [
-  {
-  "id": 1,
-  "title": "Learn React",
-  "body": "just trymmma list aout an example"
-}, {
-  "id": 2,
-  "title": "Learn React",
-  "body": "Its quite popular"
-}, {
-  "id": 3,
-  "title": "Learn Python",
-  "body": "Its endearing"
-}, {
-  "id": 4,
-  "title": "Learn Vue",
-  "body": "it graphical"
-}
-]
+
 
 
 class App extends Component{
   state = {
-    todos:[]
+    todos: []
   };
   componentDidMount(){
     this.getTodos();
   }
   getTodos(){
     axios
-      .get('http:/12.0.0.1:8000/api/todos/')
+      .get('http://127.0.0.1:8000/api/todo/')
       .then(res => {
         this.setState({
           todos:res.data
