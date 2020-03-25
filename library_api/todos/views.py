155caa9d3145129f3a1aa5f307d39d049f7ce954
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-03-25 13:01:13 +0300

    done with views

--- library_api/todos/views.py
+++ library_api/todos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+class ListTodo(ListView):
+    model = 
+    context_object_name = ''
+    template_name=''
