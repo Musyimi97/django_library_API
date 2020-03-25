69177143a028939d26403c0c0186506eda9134c7
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-03-25 09:10:32 +0300

    created urls and linked to the main thing

--- library_api/books/views.py
+++ library_api/books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+class ModelNameList(ListView):
+    model = Book
+    context_object_name = 'book'
+    template_name='book_list.html'
