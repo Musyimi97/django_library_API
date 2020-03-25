4cad3b00329dc0c683c00488251dfbaba424b38b
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-03-25 08:34:12 +0300

    created new model fields

--- library_api/books/models.py
+++ library_api/books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title= models.CharField(max_legth=250)
+    subtitle = models.CharField(max_length=250)
+    author = models.CharField(max_length=150)
+    isbn = model.CharField(max_length=13)
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
