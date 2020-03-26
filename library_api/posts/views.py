4a8d1de6db9923f06aee3a86b25bb472f862038a
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-03-26 20:48:54 +0300

    completed adding viewsets to views at Post

--- library_api/posts/views.py
+++ library_api/posts/views.py
@@ -3,8 +3,18 @@ from django.contrib.auth import get_user_model
 from .models import Post
 from .serializers import PostSerializers, UserSerializer
 from .permissions import IsAuthourReadOnly
+from rest_framework import viewsets
 
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes= (IsAuthourReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset=get_user_model().objects.all()
+    serializer_class = UserSerializer
 class PostList(generics.ListCreateAPIView):
     permission_classes = (IsAuthourReadOnly),
     queryset= Post.objects.all()
