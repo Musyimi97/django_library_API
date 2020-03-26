12d0a79e9188293b9f273c49830dcb03e326ac07
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-03-26 10:41:15 +0300

    ReadOnly Perm

--- library_api/posts/views.py
+++ library_api/posts/views.py
@@ -1,14 +1,15 @@
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializers
+from .permissions import IsAuthourReadOnly
 
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated),
+    permission_classes = (IsAuthourReadOnly),
     queryset= Post.objects.all()
     serializer_class = PostSerializers
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated),
+    permission_classes = (IsAuthourReadOnly),
     queryset = Post.objects.all()
     serializer_class =PostSerializers
